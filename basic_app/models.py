070aae5255d81a496aa005513edbdc771b42bcac
Mohammad Khatibzadeh <mohammad.khatibzadeh.3@gmail.com>
2020-03-22 22:22:41 +0430

    User_Login

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# class UserProfileInfo(models.Model):
+    
+#     user = models.OneToOneField(User)
+#     #additional classes
+#     portfolio_site = models.URLField(blank=True)
+
+#     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+#     def __str__(self):
+#         return self.user.username
+
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+    user_job = models.CharField(max_length=120)
+    national_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
+class PCategory(models.Model):
+    cat_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cat_name
+
+class Products(models.Model):
+    category = models.ForeignKey(PCategory, on_delete=models.CASCADE, related_name='products')
+    product_name = models.CharField(max_length=100)
+    product_image = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.product_name
--- basic_app/views.py
+++ basic_app/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from .forms import UserForm, UserProfileInfoForm
+
+
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView
 
 # Create your views here.
+
+
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+
+
+
+def registration(request):
+    registerd = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data = request.POST)
+        user_profile = UserProfileInfoForm(data = request.POST)
+
+        if user_form.is_valid() and user_profile.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+
+            profile = user_profile.save(commit= False)
+            profile.user = user
+            profile.save()
+
+            registerd = True
+        else:
+            print(user_form.errors, user_profile.errors)
+
+    else:
+        user_form = UserForm()
+        user_profile = UserProfileInfoForm()
+    
+    return render(request, 'registration.html', context={'registerd':registerd, 'user_form':user_form, 'user_profile':user_profile})
+
+
